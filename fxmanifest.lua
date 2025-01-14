e7031d0037605ee4031e24da21bf2a113fbec1ed
ic3d00 <guilhermerodriguesousa@gmail.com>
2025-01-14 12:23:05 +0000

    chore: bump manifest version to v1.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 author 'ic3d_'
 discord 'https://discord.gg/3h2WpSH7Sg'
 description 'Wind Turbines Worker job by ic3d_'
-version '1.0.0'
+version '1.0.1'
 client_script {'client/main.lua'}
 server_scripts {'server/main.lua'}
 shared_script {'config/shared.lua','@ox_lib/init.lua',}
